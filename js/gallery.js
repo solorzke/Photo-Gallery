8ac470a4445b501c6f85821a754147b51d974d28
Kevin Solorzano <solorzke@gmail.com>
2018-03-29 19:37:57 -0400

    Open JSON File & get images

--- js/gallery.js
+++ js/gallery.js
@@ -54,7 +54,7 @@ var mImages = [];
 
 // Holds the retrived JSON information
 var mJson = JSON.parse(mRequest.responseText);
-
+mRequest.open("GET", mURL, true);
 // URL for the JSON to load by default
 // Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
 var mUrl = 'insert_url_here_to_image_json';
