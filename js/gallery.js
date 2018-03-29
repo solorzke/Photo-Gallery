117355792d3653e0bab608ec2e672ebe6ec92a75
Kevin Solorzano <solorzke@gmail.com>
2018-03-29 19:59:02 -0400

    Added PrevPhoto() to go back the last image

--- js/gallery.js
+++ js/gallery.js
@@ -35,24 +35,39 @@ function animate() {
 var mCurrentIndex = null;
 
 function swapPhoto() {
-	if(mCurrentIndex == null){
-		mCurrentIndex = 0;
-	}
+	if(mCurrentIndex == null){ mCurrentIndex = 0; }
 
-	else if (mCurrentIndex ==  mImages.length - 1) {
-		mCurrentIndex = 0;
-	}
+	else if (mCurrentIndex ==  mImages.length - 1) { mCurrentIndex = 0; }
 
-	else{
-		mCurrentIndex++;
-		console.log(mCurrentIndex);
-	}
+	else { mCurrentIndex++; }
 
 	const location = "Location: ", description = "Description: ", date = "Date: ";
 	var photoLocation = mImages[mCurrentIndex].imgLocation;
 	var photoDesc = mImages[mCurrentIndex].description;
 	var photoDate = mImages[mCurrentIndex].date;
 	console.log('swap photo');
+	$("#photo").attr("src", mImages[mCurrentIndex].imgPath);
+	console.log(mImages[mCurrentIndex].imgPath);
+	$(".location").text(location + photoLocation);
+	$(".description").text(description + photoDesc);
+	$(".date").text(date + photoDate);
+
+	console.log(photoLocation);
+	console.log(mCurrentIndex + " mCurrentIndex on swapPhoto");
+}
+
+function PrevPhoto(){
+
+	if(mCurrentIndex == null){ mCurrentIndex = mImages.length - 1; }
+
+	else if(mCurrentIndex == 0){ mCurrentIndex = mImages.length - 1; }
+
+	else { mCurrentIndex--; }
+
+	const location = "Location: ", description = "Description: ", date = "Date: ";
+	var photoLocation = mImages[mCurrentIndex].imgLocation;
+	var photoDesc = mImages[mCurrentIndex].description;
+	var photoDate = mImages[mCurrentIndex].date;
 	$("#photo").attr("src", mImages[mCurrentIndex].imgPath);
 	console.log(mImages[mCurrentIndex].imgPath);
 	$(".location").text(location + photoLocation);
@@ -72,19 +87,19 @@ var mRequest = new XMLHttpRequest();
 var mImages = [];
 
 // Holds the retrived JSON information
-mRequest2.onreadystatechange = function() {
+mRequest.onreadystatechange = function() {
   // Do something interesting if file is opened successfully
-  if (mRequest2.readyState == 4 && mRequest2.status == 200) {
+  if (mRequest.readyState == 4 && mRequest.status == 200) {
     try {
       // Let’s try and see if we can parse JSON
-      var mJson2 = JSON.parse(mRequest2.responseText);
+      var mJson = JSON.parse(mRequest.responseText);
 
-      for (var i = 0; i < mJson2.images.length; i++){
-      	mImages.push(new GalleryImage(mJson2.images[i].imgPath, mJson2.images[i].imgLocation, mJson2.images[i].description, mJson2.images[i].date));
+      for (var i = 0; i < mJson.images.length; i++){
+      	mImages.push(new GalleryImage(mJson.images[i].imgPath, mJson.images[i].imgLocation, mJson.images[i].description, mJson.images[i].date));
       }
       // Let’s print out the JSON; It will likely show as “obj”
-      console.log(mJson2.images);
-      console.log(mJson2.images[0].imgPath);
+      console.log(mJson.images);
+      console.log(mJson.images[0].imgPath);
 
      
     } catch (err) {
@@ -95,11 +110,6 @@ mRequest2.onreadystatechange = function() {
 
 mRequest.open("GET", mURL, true);
 mRequest.send();
-
-// URL for the JSON to load by default
-// Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
-var mUrl = 'insert_url_here_to_image_json';
-
 
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
 //@param A GalleryImage object. Use this method for an event handler for loading a gallery Image object (optional).
